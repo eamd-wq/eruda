1c4db5d3df5bb2eab22057cccf6c2e2abcd679d8
shuang2.zheng <shaung2.zheng@ly.com>
2026-09-02 15:26:14 +0800

    build: add versioned bundle output

--- build/build.js
+++ build/build.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { copyFileSync } = require('fs')
 const fs = require('licia/fs')
 
 const pkg = require('../package.json')
@@ -9,5 +10,14 @@ delete pkg.devDependencies
 fs.writeFile(
   path.resolve(__dirname, '../dist/package.json'),
   JSON.stringify(pkg, null, 2),
-  'utf8'
+  'utf8',
+)
+
+/**
+ * Keep the canonical filename for compatibility and add a versioned bundle for
+ * direct distribution.
+ */
+copyFileSync(
+  path.resolve(__dirname, '../dist/eruda.js'),
+  path.resolve(__dirname, `../dist/eruda-${pkg.version}.js`),
 )
